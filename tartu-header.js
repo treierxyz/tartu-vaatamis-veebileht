3188a4bb12eca53009e0dbe2866d7966c4c04cb7
treierxyz <oliver.jogar@ut.ee>
2022-11-16 12:31:08 +0200

    Header valmis

--- tartu-header.js
+++ tartu-header.js
@@ -1,6 +1,7 @@
 // Koodi kirjutas Oliver Jõgar
 // Isetehtud elemendid: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
 // Fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// CSS linkimine: https://stackoverflow.com/questions/574944/how-to-load-up-css-files-using-javascript
 
 class TartuHeader extends HTMLElement {
     constructor() {
@@ -9,7 +10,12 @@ class TartuHeader extends HTMLElement {
 
     async connectedCallback() {
         let html = await fetch('/tartu-header.html')
+        var link = document.createElement('link');
+        link.href = "/tartu-header.css";
+        link.type = "text/css";
+        link.rel = "stylesheet";
         this.innerHTML = await html.text();
+        this.appendChild(link);
     }
 }
 
