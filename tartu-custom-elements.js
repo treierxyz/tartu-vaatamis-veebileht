0e46898af30f783d0612f6d684852582551dc8fe
treierxyz <me@treier.xyz>
2022-12-04 22:29:57 +0200

    Lihtsam CSS link kohandatud elementides

--- tartu-custom-elements.js
+++ tartu-custom-elements.js
@@ -1,7 +1,6 @@
 // Koodi kirjutas Oliver Jõgar
 // Isetehtud elemendid: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
 // Fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-// CSS linkimine: https://stackoverflow.com/questions/574944/how-to-load-up-css-files-using-javascript
 
 // päis
 class TartuHeader extends HTMLElement { // defineerime kohandatud elemendi jaoks uue klassi
@@ -11,12 +10,7 @@ class TartuHeader extends HTMLElement { // defineerime kohandatud elemendi jaoks
 
     async connectedCallback() { // connectedCallback kutsutakse iga kord kui kohandatud element lisatakse DOMi
         let html = await fetch('./tartu-header.html') // fetch võimaldab ressursse hankida, sel juhul päise HTML sisu
-        var link = document.createElement('link'); // tekitame uue link elemendi
-        link.href = "./tartu-header.css"; // lisame sellele CSS faili viitena
-        link.type = "text/css"; // seame faili tüübiks CSS
-        link.rel = "stylesheet"; // määrab faili ülesande, sel juhul kohandatud elemendi stiilimine
         this.innerHTML = await html.text(); // oodatakse kuni HTML fail on hangitud ning sisu lisatakse elemendi innerHTMLi
-        this.appendChild(link); // varem koostatud link element lisatakse juurde stiilimiseks
     }
 }
 
@@ -28,12 +22,7 @@ class TartuFooter extends HTMLElement { // analoogne töökäik päise klassiga
 
     async connectedCallback() {
         let html = await fetch('./tartu-footer.html')
-        var link = document.createElement('link');
-        link.href = "./tartu-footer.css";
-        link.type = "text/css";
-        link.rel = "stylesheet";
         this.innerHTML = await html.text();
-        this.appendChild(link);
     }
 }
 
